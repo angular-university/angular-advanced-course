c387229921fa8f5cfba5d29a62476886f0138eea
JHades <jhades.dev@gmail.com>
2017-05-02 18:12:02 +0200

    small component example

--- au-input-box/src/app/app.component.spec.ts
+++ au-input-box/src/app/app.component.spec.ts
@@ -21,6 +21,14 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
+
+  it('should render a font-awesome email button', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.fa.icon.fa-envelope')).toBeTruthy();
+  }));
+
   /*
 
   it(`should have as title 'app works!'`, async(() => {
