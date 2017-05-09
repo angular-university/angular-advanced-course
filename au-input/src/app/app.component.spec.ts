ab70b952707272b54ae823ab4161fe1313de59da
Your Name <you@example.com>
2017-05-09 13:43:10 +0200

    testing

--- au-input/src/app/app.component.spec.ts
+++ au-input/src/app/app.component.spec.ts
@@ -1,24 +1,26 @@
-import { TestBed, async } from '@angular/core/testing';
+import {TestBed, async} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {AuFaInputComponent} from "./lib/au-fa-input/au-fa-input.component";
+import {InputRefDirective} from "./lib/common/input-ref.directive";
 
-import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent, AuFaInputComponent, InputRefDirective
+            ],
+        }).compileComponents();
+    }));
 
+    it('should create the app', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
 
+        const app = fixture.debugElement.componentInstance;
 
+        expect(app).toBeTruthy();
+    }));
 
 
 
