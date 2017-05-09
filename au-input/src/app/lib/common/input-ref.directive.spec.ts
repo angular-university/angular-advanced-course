44a0971125950e1057b253001b75dcc845a5c8fe
Your Name <you@example.com>
2017-05-09 11:30:22 +0200

    testing

--- au-input/src/app/lib/common/input-ref.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InputRefDirective } from './input-ref.directive';
-
-describe('InputRefDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InputRefDirective();
-    expect(directive).toBeTruthy();
-  });
-});
