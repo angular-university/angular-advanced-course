3a84dd47f0c312ea6a42e43f594309eb82852b8e
Your Name <you@example.com>
2017-05-09 15:06:16 +0200

    exercise material design input

--- au-input/src/app/lib/au-md-input/au-md-input.component.ts
+++ au-input/src/app/lib/au-md-input/au-md-input.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, ContentChild, AfterContentInit, HostBinding} from '@angular/core';
+import {InputRefDirective} from "../common/input-ref.directive";
+
 
 @Component({
-  selector: 'au-md-input',
-  templateUrl: './au-md-input.component.html',
-  styleUrls: ['./au-md-input.component.css']
+    selector: 'au-md-input',
+    templateUrl: './au-md-input.component.html',
+    styleUrls: ['./au-md-input.component.css']
 })
-export class AuMdInputComponent implements OnInit {
+export class AuMdInputComponent implements AfterContentInit {
 
-  constructor() { }
+    @Input()
+    icon: string;
 
-  ngOnInit() {
-  }
+    @ContentChild(InputRefDirective)
+    input: InputRefDirective;
+
+    ngAfterContentInit() {
+        if (!this.input) {
+            console.error("the au-md-input needs an input inside its content");
+        }
+    }
+
+    @HostBinding('class.input-focus')
+    get isInputFocus() {
+        return this.input ? this.input.focus : false;
+    }
+
 
 }
