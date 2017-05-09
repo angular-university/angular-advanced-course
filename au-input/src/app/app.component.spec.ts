874e44d9e5ccd7a4b66c379cc9bddb15bfdecc32
Your Name <you@example.com>
2017-05-09 14:18:38 +0200

    testing

--- au-input/src/app/app.component.spec.ts
+++ au-input/src/app/app.component.spec.ts
@@ -1,10 +1,17 @@
-import {TestBed, async} from '@angular/core/testing';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {AuFaInputComponent} from "./lib/au-fa-input/au-fa-input.component";
 import {InputRefDirective} from "./lib/common/input-ref.directive";
-
+import {DebugElement} from '@angular/core';
+import {By}              from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+
+    let component: AppComponent,
+        fixture: ComponentFixture<AppComponent>,
+        el: DebugElement,
+        emailField: DebugElement;
+
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -14,12 +21,34 @@ describe('AppComponent', () => {
         }).compileComponents();
     }));
 
-    it('should create the app', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
+    beforeEach(() => {
 
-        const app = fixture.debugElement.componentInstance;
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.debugElement.componentInstance;
+        el = fixture.debugElement;
+        emailField = el.query(By.css('#email-field'));
 
-        expect(app).toBeTruthy();
+        fixture.detectChanges();
+
+    });
+
+
+    it('should create the test application', async(() => {
+        expect(component).toBeTruthy();
+    }));
+
+
+    it('should create a font awesome email input', async(() => {
+        expect(emailField).toBeTruthy();
+
+    }));
+
+    it('should include the correct email icon inside the email input', async(() => {
+        expect(emailField.query(By.css('i.icon.fa.fa-envelope'))).toBeTruthy();
+    }));
+
+    it('should have projected the correct test input inside the email field', async(() => {
+        expect(emailField.query(By.css('input.test-class'))).toBeTruthy();
     }));
 
 
