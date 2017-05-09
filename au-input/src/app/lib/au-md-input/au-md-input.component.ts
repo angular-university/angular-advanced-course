afc4305124cf1bf13eead719bb13b3597d944df4
Your Name <you@example.com>
2017-05-09 14:49:02 +0200

    exercise material design input

---
+++ au-input/src/app/lib/au-md-input/au-md-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'au-md-input',
+  templateUrl: './au-md-input.component.html',
+  styleUrls: ['./au-md-input.component.css']
+})
+export class AuMdInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
