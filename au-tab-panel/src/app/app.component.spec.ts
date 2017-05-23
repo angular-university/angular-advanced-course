2ec903733934ef812e95020b4cb12891eb112ddc
JHades <jhades.dev@gmail.com>
2017-05-23 11:37:10 +0200

    au-modal

--- au-tab-panel/src/app/app.component.spec.ts
+++ au-tab-panel/src/app/app.component.spec.ts
@@ -1,32 +1,54 @@
 import { TestBed, async } from '@angular/core/testing';
-
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
+import {DebugElement} from '@angular/core';
+import {By}              from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import {TabPanelComponent} from "./tab-panel/tab-panel.component";
+import {TabComponent} from "./tab/tab.component";
+
 
 describe('AppComponent', () => {
+
+  let component: AppComponent,
+    fixture: ComponentFixture<AppComponent>,
+    el: DebugElement,
+    emailField: DebugElement;
+
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, TabPanelComponent, TabComponent
       ],
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  beforeEach(() => {
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+    el = fixture.debugElement;
+    emailField = el.query(By.css('#email-field'));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+
+  });
+
+
+  it('should create the test application', async(() => {
+    expect(component).toBeTruthy();
   }));
+
+
+  it('should include the correct email icon inside the email input', async(() => {
+
+    const tabs = emailField.queryAll(By.css('au-tab-pane .tab'));
+
+    //TODO expect(tabs).toBeTruthy();
+    //TODO expect(tabs.length).toBe(1);
+  }));
+
+
+
+
 });
