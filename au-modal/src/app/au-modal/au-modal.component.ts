c16c727b1d5a358accf16e5bb5ef73b45fc95d83
Your Name <you@example.com>
2017-05-23 15:46:49 +0200

    recording

---
+++ au-modal/src/app/au-modal/au-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'au-modal',
+  templateUrl: './au-modal.component.html',
+  styleUrls: ['./au-modal.component.scss']
+})
+export class AuModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- au-modal/src/app/au-modal/au-modal.module.ts
+++ au-modal/src/app/au-modal/au-modal.module.ts
@@ -4,15 +4,16 @@
 
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuModalComponent } from './au-modal.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AuModalComponent],
   imports: [
     CommonModule
   ],
-  exports: []
+  exports: [AuModalComponent]
 })
 export class AuModalModule {
 
