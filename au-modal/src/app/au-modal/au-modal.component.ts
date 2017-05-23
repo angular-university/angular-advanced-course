e92fab8f7b4b6b43e19b469d520d82a3e0b60f6d
Your Name <you@example.com>
2017-05-23 16:21:11 +0200

    recording

--- au-modal/src/app/au-modal/au-modal.component.ts
+++ au-modal/src/app/au-modal/au-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, TemplateRef} from '@angular/core';
 
 @Component({
   selector: 'au-modal',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./au-modal.component.scss']
 })
 export class AuModalComponent implements OnInit {
+
+  @Input()
+  body: TemplateRef<any>;
 
   constructor() { }
 
