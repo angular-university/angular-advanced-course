7bc0ccfc0e8b001d47c1becac08e0fc51a018303
Your Name <you@example.com>
2017-05-30 15:22:09 +0200

    recording

--- au-modal/src/app/au-modal/au-modal.component.ts
+++ au-modal/src/app/au-modal/au-modal.component.ts
@@ -12,14 +12,37 @@ export class AuModalComponent implements OnInit {
     @Input()
     body: TemplateRef<any>;
 
+    @Input()
+    hideOnEsc = true;
 
-    constructor(private modalService: AuModalService) {
+    @Input()
+    hideOnClickOutside = true;
+
+
+    constructor(private modalService: AuModalService,
+                private eventManager: EventManager) {
+
+
 
     }
 
     ngOnInit() {
+
+        this.eventManager.addGlobalEventListener("window",'keyup.esc', () => {
+            if (this.hideOnEsc) {
+                this.closeModal();
+            }
+        });
+
     }
 
+
+    onClickOutsideModal() {
+        if (this.hideOnClickOutside) {
+            this.closeModal();
+        }
+
+    }
 
     closeModal() {
         this.modalService.close();
