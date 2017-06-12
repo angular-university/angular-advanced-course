5128a1b13221c82e16cb0976f418666f26d24343
JHades <jhades.dev@gmail.com>
2017-06-12 13:14:41 +0200

    preparation final section

--- animations/src/app/app.module.ts
+++ animations/src/app/app.module.ts
@@ -1,20 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AuInputModule} from "au-input";
+import {AuTabPanelModule} from 'au-tab-panel';
+import {AuMaskModule} from "au-mask";
+import {AuModalModule} from 'au-modal';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AuInputModule,
+        AuTabPanelModule,
+        AuMaskModule,
+        AuModalModule.forRoot()
+
+
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
