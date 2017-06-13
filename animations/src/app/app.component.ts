7ba08363702c7b079d9da61fcf13bb95132a619a
Your Name <you@example.com>
2017-06-13 11:11:49 +0200

    au-mask

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -1,21 +1,12 @@
 import {Component} from '@angular/core';
-import {fadeInOut} from "./fade-in-out.animation";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    animations: [fadeInOut]
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
 
-    onAnimationStart() {
-        console.log("On Animation Start");
-    }
-
-    onAnimationEnd() {
-        console.log("On Animation End");
-    }
 
 }
