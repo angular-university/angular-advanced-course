cb9dff013038a4d94b89ca02431f4f77d4808ecc
JHades <jhades.dev@gmail.com>
2019-06-06 12:59:04 +0200

    upgrade to Angular 8 and Node 12

--- au-input/src/app/app.module.ts
+++ au-input/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
-import 'rxjs/add/operator/map';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -13,7 +12,7 @@ import 'rxjs/add/operator/map';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
